4a5f6ac51539a42c9548fce68d619acbac854586
hieupt <trunghieupham03@gmail.com>
2023-04-20 07:26:28 +0700

    #

--- src/components/NavTop.tsx
+++ src/components/NavTop.tsx
@@ -1,101 +1,22 @@
-// import Image from "next/image";
-// import storage from "local-storage-fallback";
-// import { useRouter } from "next/router";
-// import { BsSearch } from "react-icons/bs";
-// import { IoMdNotificationsOutline } from "react-icons/io";
-
-// export async function getServerSideProps(req: any, res: any) {
-//   const response = await fetch("http://localhost:3000/api/auth");
-//   const tokenStore = await response.json();
-//   return {
-//     props: {
-//       tokenStore,
-//     },
-//   };
-// }
-
-// export default function NavTop(tokenStore: any) {
-//   const router = useRouter();
-
-//   return (
-//     <div className="flex justify-between items-center px-6 w-full">
-//       <div className="flex items-center w-1/3">
-//         <button>
-//           <BsSearch size={25} />
-//         </button>
-//         <input
-//           type="search"
-//           name=""
-//           id=""
-//           className="bg-[#f0eee3] p-4 w-full text-base outline-none border-none ml-2"
-//           placeholder="Tìm sách, tác giả, ..."
-//         />
-//       </div>
-//       <div className="flex space-x-12">
-//         {tokenStore.length > 10 ? (
-//           <div
-//             onClick={() => {
-//               storage.setItem("token", "none");
-//               router.reload();
-//             }}
-//             className="flex items-center justify-center hover:bg-[#C9C39F] cursor-pointer px-4 py-2 rounded-full"
-//           >
-//             <Image
-//               src="/BlankAvatar.jpg"
-//               height={30}
-//               width={30}
-//               alt={"Avatar"}
-//               priority={true}
-//               className="rounded-full"
-//             />
-//             <span className="ml-3 text-base font-semibold">Username</span>
-//           </div>
-//         ) : (
-//           <div className="flex items-center bg-[#C9C39F] rounded-full">
-//             <button
-//               onClick={() => {
-//                 router.push("/login");
-//               }}
-//               className="px-4"
-//             >
-//               Đăng nhập
-//             </button>
-//           </div>
-//         )}
-//         <button className="p-2 hover:bg-[#C9C39F] rounded-full">
-//           <IoMdNotificationsOutline size={30} />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { BsSearch } from "react-icons/bs";
+import { useState, useEffect } from "react";
+import { getCookie, deleteCookie } from "cookies-next";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import Cookies from "js-cookie";
-import { GetServerSideProps } from "next";
 
-type Props = {
-  tokenStore: string;
-};
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => {
-  const tokenStore = Cookies.get("token") || "";
-  const cookie = req.headers.cookie;
-  console.log(cookie);
-
-  return {
-    props: {
-      tokenStore,
-    },
-  };
-};
-
-export default function NavTop({ tokenStore }: { tokenStore: string }) {
+export default function NavTop() {
   const router = useRouter();
-  console.log(Cookies.get("token"));
+
+  const [authCookie, setAuthCookie] = useState("");
+  useEffect(() => {
+    function getAuthCookie() {
+      const getToken = getCookie("token");
+      if (typeof getToken === "string") setAuthCookie(getToken);
+    }
+    getAuthCookie();
+  }, []);
+
   return (
     <div className="flex justify-between items-center px-6 w-full">
       <div className="flex items-center w-1/3">
@@ -111,10 +32,10 @@ export default function NavTop({ tokenStore }: { tokenStore: string }) {
         />
       </div>
       <div className="flex space-x-12">
-        {tokenStore && tokenStore.length > 10 ? (
+        {authCookie !== "" ? (
           <div
             onClick={() => {
-              Cookies.remove("token");
+              deleteCookie("token", { path: "" });
               router.reload();
             }}
             className="flex items-center justify-center hover:bg-[#C9C39F] cursor-pointer px-4 py-2 rounded-full"
--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,51 +1,11 @@
-// import { User } from "@/models/User";
-// import storage from "local-storage-fallback";
-
-// export default async function handler(req: any, res: any) {
-//   const baseURL = "https://localhost:7021/api/Auth";
-
-//   switch (req.method) {
-//     case "POST":
-//       try {
-//         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-//         const usr: User = req.body;
-//         const response = await fetch(baseURL + "/login", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Accept: " text/json",
-//           },
-//           body: JSON.stringify({
-//             username: usr.username,
-//             password: usr.password,
-//           }),
-//         });
-//         const AuthToken = await response.json();
-//         storage.setItem("token", AuthToken.token);
-//         res.redirect(307, "/");
-//       } catch (err) {
-//         console.log(err);
-//         res.status(405).json({ msg: "Auth POST Method had error" });
-//       } finally {
-//         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
-//       }
-//       break;
-
-//     case "GET":
-//       const tokenStore = storage.getItem("token");
-//       res.json({ token: tokenStore });
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
-
 import { User } from "@/models/User";
+import { setCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
-import cookie from "cookie";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const baseURL = "https://localhost:7021/api/Auth";
 
   switch (req.method) {
@@ -65,28 +25,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           }),
         });
         const authToken = await response.json();
-        res.setHeader(
-          "Set-Cookie",
-          cookie.serialize("token", authToken.token, {
-            httpOnly: false,
-            secure: false,
-            maxAge: 60 * 60 * 24 * 7, // 1 week
-            path: "/",
-          })
-        );
+        setCookie("token", authToken.token, { req, res, maxAge: 60 * 60 * 24 });
         res.redirect(307, "/");
       } catch (err) {
         console.log(err);
-        res.status(405).json({ msg: "Auth POST Method had error" });
+        res.status(405).json({ msg: "Auth POST method had error(s)." });
       } finally {
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
       }
-      break;
-
-    case "GET":
-      const cookies = cookie.parse(req.headers.cookie || "");
-      const token = cookies.token;
-      res.json({ token: token });
       break;
 
     default:
