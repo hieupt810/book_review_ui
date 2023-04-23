2da9c88a0b60ff82920d2943dfd322a26f7015d3
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2023-04-23 11:34:38 +0700

    .

--- src/components/ListBook.tsx
+++ src/components/ListBook.tsx
@@ -1,18 +1,40 @@
 import useSWR from "swr";
-import { Book } from "@/models/Book";
 import { FC } from "react";
-
-const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
+import Image from "next/image";
+import { Book } from "@/models/Book";
 
 interface ListBookProps {}
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const ListBook: FC<ListBookProps> = ({}) => {
   const { data, error } = useSWR("http://localhost:3000/api/booklist", fetcher);
 
+  if (error) {
+    return <div>Failed to load data.</div>;
+  }
+
+  if (!data) {
+    return <div>Loading data...</div>;
+  }
+  console.log(data);
   return (
     <div>
       <h3 className="text-2xl text-gray-900 leading-6 my-4">Kho sách</h3>
-      {}
+      {data.data.map((item: Book, index: any) => (
+        <li key={index}>
+        <p>Book Name: {item.title}</p>
+        <p>Author: {item.author}</p>
+        {/* {item.reviews.map((review, index) => (
+            <div key={index}>
+              <p>Review Content: {review.content}</p>
+              <p>Rating: {review.rating}</p>
+              <p>Created Date: {review.createdDate}</p>
+              {review.user && <p>Username: {review.user.username}</p>}
+            </div>
+          ))} */}
+      </li>
+      ))}
     </div>
   );
 };
--- src/pages/api/booklist.ts
+++ src/pages/api/booklist.ts
@@ -1,6 +1,7 @@
 import { getCookie } from "cookies-next";
 
 export default async function handler(req: any, res: any) {
+  console.log(req.cookies); // log out all cookies
   const baseURL = "https://localhost:7021/book/all";
   try {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -8,11 +9,13 @@ export default async function handler(req: any, res: any) {
       method: "GET",
       headers: {
         Accept: "text/json",
-        Authorization: "Bearer " + getCookie("token"),
+        Authorization: "Bearer " + req.cookies.token, // access token from cookies object
         "Content-Type": "text/json",
       },
     });
-    // console.log(response);
+    //console.log(req.cookies.token); // log out token value
+    return res.status(200).json(await response.json());
+
   } catch (error) {
     console.log(error);
     res.status(405).json({ msg: "Review GET Method had error" });
